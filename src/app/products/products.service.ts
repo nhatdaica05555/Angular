b508ea1a5a9e82b780195058f3838d696400726f
nhat <buidinhnhat2010@gmail.com>
2018-07-15 09:36:21 +0700

    fixed

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductsService {
   getProducts() : string[] {
-    return ['sp1' , 'sp2' , 'sp3' ,'sp34' , 'sp32'];
+    return ['sp1' , 'sp2' , 'sp3' ,'sp34' , 'sp32','23'];
   }
 
   getId() : number{
-    return 1;
+    return 2;
   }
   constructor() { }
 }
