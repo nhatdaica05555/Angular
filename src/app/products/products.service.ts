cd42c4e40c1a588ae26f3845c22f7ea25d275fd7
nhat <buidinhnhat2010@gmail.com>
2018-07-15 09:16:57 +0700

    added sp34

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductsService {
   getProducts() : string[] {
-    return ['sp1' , 'sp2' , 'sp3'];
+    return ['sp1' , 'sp2' , 'sp3' ,'sp34' , 'sp32'];
   }
   constructor() { }
 }
